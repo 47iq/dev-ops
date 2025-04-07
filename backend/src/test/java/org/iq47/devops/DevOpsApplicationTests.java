09513cb40c635772744b5b5578097d2e8e34f421
Ivan Kustarev <ivan.kustarev@tune-it.ru>
2025-04-07 22:47:19 +0300

    feature: create example test

--- backend/src/test/java/org/iq47/devops/DevOpsApplicationTests.java
+++ backend/src/test/java/org/iq47/devops/DevOpsApplicationTests.java
@@ -1,13 +1,12 @@
 package org.iq47.devops;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class DevOpsApplicationTests {
-
     @Test
-    void contextLoads() {
+    void testTest(){
+        Assertions.assertTrue(true);
     }
-
 }
