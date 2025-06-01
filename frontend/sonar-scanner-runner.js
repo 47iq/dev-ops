c71564c7b54a83f026915c40e9ebdc289ee4b892
Ivan Kustarev <ivan.kustarev@tune-it.ru>
2025-06-01 21:59:08 +0300

    in progress

---
+++ frontend/sonar-scanner-runner.js
@@ -0,0 +1,18 @@
+const scanner = require('sonarqube-scanner').default || require('sonarqube-scanner');
+
+scanner(
+    {
+        serverUrl: process.env.SONAR_HOST_URL || 'http://localhost:9000',
+        options: {
+            'sonar.projectKey': 'devops-frontend',
+            'sonar.projectName': 'devops-frontend',
+            'sonar.sources': 'src',
+            'sonar.tests': 'test',
+            'sonar.inclusions': 'src/**/*.js',
+            'sonar.test.inclusions': 'test/**/*.test.js',
+            'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
+            'sonar.login': process.env.SONAR_AUTH_TOKEN || '',
+        },
+    },
+    () => process.exit()
+);
