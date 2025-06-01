8b1c97fbec3189412d5cdfc45411d882fee16a95
Ivan Kustarev <ivan.kustarev@tune-it.ru>
2025-06-01 22:43:35 +0300

    add jenkins for bot

---
+++ jenkins/jobs/pipeline-bot.groovy
@@ -0,0 +1,18 @@
+pipelineJob('pipeline-bot') {
+    triggers {
+        scm('H/1 * * * *')
+    }
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/47iq/dev-ops.git')
+                    }
+                    branches('main', 'lab1', 'lab2', 'lab3', 'lab4')
+                }
+            }
+            scriptPath('./backend/Jenkinsfile')
+        }
+    }
+}
